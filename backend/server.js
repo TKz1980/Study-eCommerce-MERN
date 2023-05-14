d3d9376eda08f76ef05091a5c58ed5cdc23c9b3c
Takashi <takashi.zume@gmail.com>
2023-05-14 13:47:25 +0900

    Deploy preparation

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -40,11 +37,25 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
-
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
